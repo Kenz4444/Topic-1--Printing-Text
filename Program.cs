7f4696c0b1ea40a12300fb2d1f55d9a1fb9cb655
362574782 <362574782@2260I-L29-006>
2026-02-03 13:05:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Topic_1__Printing_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kenzie
+            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("My name is Kenzie");
+            Console.WriteLine();
+            Console.Write("Computers");
+            Console.WriteLine(" Rule");
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+
+            Console.ResetColor();
+
+
+        }
+    }
+}
